44b9e6896b36e036cd446a628f573d75526f082f
Jack O'Neal <jack@kevinandjen.org>
2024-09-12 13:31:45 -0700

    now using three.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// Optional: Parallax Effect
-window.addEventListener("scroll", function() {
-    const scrolled = window.pageYOffset;
-    document.querySelector('.background-animation').style.backgroundPositionY = `${scrolled * 0.5}px`; // Slow parallax effect
-  });
-  
