d728306ef94b161300424f675b318728aa7114ca
Jack O'Neal <jack@kevinandjen.org>
2024-09-13 17:53:19 -0700

    Added skateboard

--- main.js
+++ main.js
@@ -1,9 +1,35 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader';
 
 const scene = new THREE.Scene();
 
+const loader = new GLTFLoader();
+
+const skateboard = loader.load(
+  '/Skateboard/Skateboard.glb',
+  function (gltf) {
+    scene.add(gltf.scene);
+
+    gltf.animations;
+    gltf.scene;
+    gltf.scenes;
+    gltf.cameras;
+    gltf.asset;
+  },
+
+	function ( xhr ) {
+
+		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+	},
+	function ( error ) {
+
+		console.log( 'An error happened' );
+
+	}
+)
 
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
@@ -18,14 +44,13 @@ camera.position.setZ(30);
 
 renderer.render( scene, camera);
 
-const geometry = new THREE.TorusGeometry(10, 3, 16, 100)
-const material = new THREE.MeshStandardMaterial( { color: 0xFF6347 } )
-const torus = new THREE.Mesh( geometry, material );
-
-scene.add(torus)
+// const geometry = new THREE.TorusGeometry(10, 3, 16, 100)
+// const material = new THREE.MeshStandardMaterial( { color: 0xFF6347 } )
+// const torus = new THREE.Mesh( geometry, material );
+// scene.add(torus);
 
 const pointLight = new THREE.PointLight(0xffffff, 1, 0, 0.01);
-pointLight.position.set(10,0,0);
+pointLight.position.set(5,0,0);
 const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambientLight);
 
@@ -62,9 +87,6 @@ Array(2000).fill().forEach(addStar)
 const controls = new OrbitControls(camera, renderer.domElement);
 scene.add(controls)
 
-// const spaceTexture = new THREE.TextureLoader().load('space.jpg');
-// scene.background = spaceTexture;
-
 const jackTexture = new THREE.TextureLoader().load('jack.jpg')
 
 const jack = new THREE.Mesh(
@@ -83,13 +105,29 @@ const moon = new THREE.Mesh(
     normalMap: normalTexture
   } )
 );
+moon.position.set(15,10,1)
 scene.add(moon)
+function moveCamera() {
+  const t = document.body.getboundingClientRect().top;
+  moon.rotate.x += 0.05;
+  moon.rotate.y += 0.075;
+  moon.rotate.z +=0.05;
+  
+  jack.rotate.x += 0.02;
+  jack.rotate.y += 0.01;
+  jack.rotate.z += 0.02;
 
+  camera.position.z = t * -0.01;
+  camera.position.x = t * -0.0002;
+  camera.position.y = t * -0.0002; 
+}
+
+document.body.onscroll = moveCamera
 function animate() {
 
-  torus.rotation.x += 0.01;
-  torus.rotation.y += 0.005;
-  torus.rotation.z += 0.01;
+  // torus.rotation.x += 0.01;
+  // torus.rotation.y += 0.005;
+  // torus.rotation.z += 0.01;
 
   controls.update();
 
